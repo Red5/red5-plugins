5ff6572d433a817c26b49bd1d9a02b50d225f657
Paul Gregoire <mondain@gmail.com>
2016-05-27 20:40:43 -0400

    Fixed security plugin

--- securityplugin/src/main/java/org/red5/server/plugin/security/SecurityPlugin.java
+++ securityplugin/src/main/java/org/red5/server/plugin/security/SecurityPlugin.java
@@ -42,105 +42,61 @@ import org.slf4j.Logger;
  */
 public class SecurityPlugin extends Red5Plugin {
 
-	private static Logger log = Red5LoggerFactory.getLogger(SecurityPlugin.class, "plugins");
-	
-	private MultiThreadedApplicationAdapter application;
-	
+    private static Logger log = Red5LoggerFactory.getLogger(SecurityPlugin.class, "plugins");
+    
+    private MultiThreadedApplicationAdapter application;
+    
 
-	public void doStart() throws Exception {
-		log.debug("Start");
-	}
+    public void doStart() throws Exception {
+        log.debug("Start");
+    }
 
-	public void doStop() throws Exception {
-		log.debug("Stop");
-	}
+    public void doStop() throws Exception {
+        log.debug("Stop");
+    }
 
-	public String getName() {
-		return "securityPlugin";
-	}
-	
-	public void setApplication(MultiThreadedApplicationAdapter app) {	
-		log.trace("Setting application adapter: {}", app);
-		this.application = app;
-	}
-	
-	//methods specific to this plug-in
-	
-	public PlaybackSecurityHandler getPlaybackSecurityHandler() {
-		PlaybackSecurityHandler ph = null;
-		try {
-			ph = (PlaybackSecurityHandler) Class.forName("org.red5.server.plugin.security.PlaybackSecurityHandler").newInstance();
-			ph.setApplication(application);
-		} catch (Exception e) {
-			log.error("PlaybackSecurityHandler could not be loaded", e);
-		}
-		return ph;		
-	}
-	
-	public PublishSecurityHandler getPublishSecurityHandler() {
-		PublishSecurityHandler ps = null;
-		try {
-			ps = (PublishSecurityHandler) Class.forName("org.red5.server.plugin.security.PublishSecurityHandler").newInstance();
-			ps.setApplication(application);
-		} catch (Exception e) {
-			log.error("PublishSecurityHandler could not be loaded", e);
-		}
-		return ps;		
-	}
-	
-	public SharedObjectSecurityHandler getSharedObjectSecurityHandler() {
-		SharedObjectSecurityHandler sh = null;
-		try {
-			sh = (SharedObjectSecurityHandler) Class.forName("org.red5.server.plugin.security.SharedObjectSecurityHandler").newInstance();
-			sh.setApplication(application);
-		} catch (Exception e) {
-			log.error("SharedObjectSecurityHandler could not be loaded", e);
-		}
-		return sh;		
-	}
-	
-	//common methods
-	
-	/**
-	 * Invokes the "onStatus" event on the client, passing our derived status.
-	 * 
-	 * @param conn
-	 * @param status
-	 */
-	public static void writeStatus(IConnection conn, StatusObject status) {
-		//make a buffer to put our data in
-		IoBuffer buf = IoBuffer.allocate(128);
-		buf.setAutoExpand(true);
-		//create amf output
-		Output out = new Output(buf);
-		//mark it as an amf object
-		buf.put(AMF.TYPE_OBJECT);
-		//serialize our status
-    	status.serialize(out);
-    	//write trailer
-		buf.put((byte) 0x00);
-		buf.put((byte) 0x00);
-		buf.put(AMF.TYPE_END_OF_OBJECT);
-		//make the buffer read to be read
-		buf.flip();
-		
-		//create an RTMP event of Notify type
-		IRTMPEvent event = new Notify(buf);
+    public String getName() {
+        return "securityPlugin";
+    }
+    
+    public void setApplication(MultiThreadedApplicationAdapter app) {   
+        log.trace("Setting application adapter: {}", app);
+        this.application = app;
+    }
+    
+    //methods specific to this plug-in
+    
+    public PlaybackSecurityHandler getPlaybackSecurityHandler() {
+        PlaybackSecurityHandler ph = null;
+        try {
+            ph = (PlaybackSecurityHandler) Class.forName("org.red5.server.plugin.security.PlaybackSecurityHandler").newInstance();
+            ph.setApplication(application);
+        } catch (Exception e) {
+            log.error("PlaybackSecurityHandler could not be loaded", e);
+        }
+        return ph;      
+    }
+    
+    public PublishSecurityHandler getPublishSecurityHandler() {
+        PublishSecurityHandler ps = null;
+        try {
+            ps = (PublishSecurityHandler) Class.forName("org.red5.server.plugin.security.PublishSecurityHandler").newInstance();
+            ps.setApplication(application);
+        } catch (Exception e) {
+            log.error("PublishSecurityHandler could not be loaded", e);
+        }
+        return ps;      
+    }
+    
+    public SharedObjectSecurityHandler getSharedObjectSecurityHandler() {
+        SharedObjectSecurityHandler sh = null;
+        try {
+            sh = (SharedObjectSecurityHandler) Class.forName("org.red5.server.plugin.security.SharedObjectSecurityHandler").newInstance();
+            sh.setApplication(application);
+        } catch (Exception e) {
+            log.error("SharedObjectSecurityHandler could not be loaded", e);
+        }
+        return sh;      
+    }
 
-		//construct a packet
-		Header header = new Header();
-		Packet packet = new Packet(header, event);
-
-		//get our stream id
-		int streamId = conn.getStreamId();
-		//set channel to "data" which im pretty sure is 3
-		header.setChannelId(3);
-		header.setTimer(event.getTimestamp()); //0
-		header.setStreamId(streamId);
-		header.setDataType(event.getDataType());
-		
-		//write to the client
-		((RTMPConnection) conn).write(packet);
-	}	
-	
 }
